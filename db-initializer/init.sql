39fcad9bf98d6a2d37d53d26c1deb34b48f356a9
ahmedAbdelrahman01 <ahmedabdelraman83@gmail.com>
2023-07-29 20:17:26 +0300

    add sql script if not exist create db

--- db-initializer/init.sql
+++ db-initializer/init.sql
@@ -1,2 +1,13 @@
-CREATE DATABASE pokemon;
+-- Connect to the master database
+USE master;
 
+-- Create the "pokemon" database if it does not exist
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'pokemon')
+BEGIN
+    CREATE DATABASE pokemon;
+END
+
+-- Switch to the "pokemon" database
+USE pokemon;
+
+-- Add additional SQL commands to set up the schema or insert initial data into tables, if needed
